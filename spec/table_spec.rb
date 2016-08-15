0e2a6b716f26ea47154a2b486d52bebc268274b0
Ravindra Sunimal Herath <ravisherath@gmail.com>
2016-08-15 13:10:18 +1000

    table can handle both valid & invalid coordinates

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -8,5 +8,21 @@ describe 'Table' do
     it 'allows coordinates' do
       @table.valid_coordinates?(0,0).eql?(true)
     end
+
+    it 'should not allow x < 0 coordinates' do
+      @table.valid_coordinates?(-1,0).eql?(false)
+    end
+
+    it 'should not allow x > 4 coordinates' do
+      @table.valid_coordinates?(5,0).eql?(false)
+    end
+
+    it 'should not allow y < 0 coordinates' do
+      @table.valid_coordinates?(0,-1).eql?(false)
+    end
+
+    it 'should not allow y > 4 coordinates' do
+      @table.valid_coordinates?(0,5).eql?(false)
+    end     
   end
 end
