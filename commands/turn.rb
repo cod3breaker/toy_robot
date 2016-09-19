e7738d5de7c5099dafba3ba2ad5babdde39324b6
Ravindra Sunimal Herath <ravisherath@gmail.com>
2016-09-19 11:52:44 +1000

    Passing all the tests for turning left

--- commands/turn.rb
+++ commands/turn.rb
@@ -1,3 +1,10 @@
 class Turn
-  
+  def execute(turning_direction)
+    current_facing_direction = Robot.instance.facing_direction
+
+    Robot.instance.facing_direction = 'SOUTH' if current_facing_direction == 'EAST'
+    Robot.instance.facing_direction = 'WEST' if current_facing_direction == 'NORTH'
+    Robot.instance.facing_direction = 'EAST' if current_facing_direction == 'SOUTH'
+    Robot.instance.facing_direction = 'NORTH' if current_facing_direction == 'WEST'
+  end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -9,9 +9,27 @@ describe 'Robot Turn' do
   end
 
   describe 'Turn Left' do
-    it 'makes the Robot turn left' do
+    it 'makes the Robot turn left when facing EAST' do
       @turn.execute('LEFT')
       expect(Robot.instance.facing_direction).to eq('SOUTH')
+    end
+
+    it 'makes the Robot turn left when facing NORTH' do
+      Robot.instance.facing_direction = 'NORTH'
+      @turn.execute('LEFT')
+      expect(Robot.instance.facing_direction).to eq('WEST')
+    end
+
+    it 'makes the Robot turn left when facing SOUTH' do
+      Robot.instance.facing_direction = 'SOUTH'
+      @turn.execute('LEFT')
+      expect(Robot.instance.facing_direction).to eq('EAST')
+    end
+
+    it 'makes the Robot turn left when facing WEST' do
+      Robot.instance.facing_direction = 'WEST'
+      @turn.execute('LEFT')
+      expect(Robot.instance.facing_direction).to eq('NORTH')
     end
   end
 
