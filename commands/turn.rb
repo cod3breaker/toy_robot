07ebed6c322cd62aa2d653a790bf35d62a5700f5
Ravindra Sunimal Herath <ravisherath@gmail.com>
2016-09-19 12:55:14 +1000

    Refactoring execute method in Turn class

--- commands/turn.rb
+++ commands/turn.rb
@@ -2,18 +2,10 @@ class Turn
   def execute(turning_direction)
     current_facing_direction = Robot.instance.facing_direction
 
-    if turning_direction == 'LEFT'
-      Robot.instance.facing_direction = 'SOUTH' if current_facing_direction == 'EAST'
-      Robot.instance.facing_direction = 'WEST' if current_facing_direction == 'NORTH'
-      Robot.instance.facing_direction = 'EAST' if current_facing_direction == 'SOUTH'
-      Robot.instance.facing_direction = 'NORTH' if current_facing_direction == 'WEST'
-    elsif turning_direction == 'RIGHT'
-      Robot.instance.facing_direction = 'EAST' if current_facing_direction == 'NORTH'
-      Robot.instance.facing_direction = 'WEST' if current_facing_direction == 'SOUTH'
-      Robot.instance.facing_direction = 'NORTH' if current_facing_direction == 'EAST'
-      Robot.instance.facing_direction = 'SOUTH' if current_facing_direction == 'WEST'
-    end
+    left_turn_directions = {"NORTH" => "WEST" , "SOUTH" => "EAST" , "EAST" => "SOUTH" , "WEST" => "NORTH"}
+    right_turn_directions = {"NORTH" => "EAST" , "SOUTH" => "WEST" , "EAST" => "NORTH" , "WEST" => "SOUTH"}
 
-
+    Robot.instance.facing_direction = left_turn_directions.fetch(current_facing_direction) if turning_direction == 'LEFT'
+    Robot.instance.facing_direction = right_turn_directions.fetch(current_facing_direction) if turning_direction == 'RIGHT'
   end
 end
