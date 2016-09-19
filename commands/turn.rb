7ab70586fd655d63eff2bafb52fd2551fcdaa35c
Ravindra Sunimal Herath <ravisherath@gmail.com>
2016-09-19 12:15:50 +1000

    Passing initial turning direction for turn right

--- commands/turn.rb
+++ commands/turn.rb
@@ -2,9 +2,15 @@ class Turn
   def execute(turning_direction)
     current_facing_direction = Robot.instance.facing_direction
 
-    Robot.instance.facing_direction = 'SOUTH' if current_facing_direction == 'EAST'
-    Robot.instance.facing_direction = 'WEST' if current_facing_direction == 'NORTH'
-    Robot.instance.facing_direction = 'EAST' if current_facing_direction == 'SOUTH'
-    Robot.instance.facing_direction = 'NORTH' if current_facing_direction == 'WEST'
+    if turning_direction == 'LEFT'
+      Robot.instance.facing_direction = 'SOUTH' if current_facing_direction == 'EAST'
+      Robot.instance.facing_direction = 'WEST' if current_facing_direction == 'NORTH'
+      Robot.instance.facing_direction = 'EAST' if current_facing_direction == 'SOUTH'
+      Robot.instance.facing_direction = 'NORTH' if current_facing_direction == 'WEST'
+    elsif turning_direction == 'RIGHT'
+      Robot.instance.facing_direction = 'EAST' if current_facing_direction == 'NORTH'
+    end
+
+
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,11 +5,11 @@ describe 'Robot Turn' do
     @turn = Turn.new
     Robot.instance.x_position = 3
     Robot.instance.y_position = 3
-    Robot.instance.facing_direction = 'EAST'
   end
 
   describe 'Turn Left' do
     it 'makes the Robot turn left when facing EAST' do
+      Robot.instance.facing_direction = 'EAST'
       @turn.execute('LEFT')
       expect(Robot.instance.facing_direction).to eq('SOUTH')
     end
@@ -30,6 +30,14 @@ describe 'Robot Turn' do
       Robot.instance.facing_direction = 'WEST'
       @turn.execute('LEFT')
       expect(Robot.instance.facing_direction).to eq('NORTH')
+    end
+  end
+
+  describe 'Turn Right' do
+    it 'makes the Robot turn right when facing NORTH' do
+      Robot.instance.facing_direction = 'NORTH'
+      @turn.execute('RIGHT')
+      expect(Robot.instance.facing_direction).to eq('EAST')
     end
   end
 
