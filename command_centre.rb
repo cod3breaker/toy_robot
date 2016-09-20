c011ccde47f5a90356e22c09d6460a2cec62cc9e
Ravindra Sunimal Herath <ravisherath@gmail.com>
2016-09-20 14:51:07 +1000

    Passing Place command for command_centre

--- command_centre.rb
+++ command_centre.rb
@@ -1,2 +1,8 @@
 class CommandCentre
+  def execute(command_array)
+    x = command_array[1]
+    y = command_array[2]
+    facing_direction = command_array[3]
+    Place.new.execute(x,y,facing_direction)
+  end
 end
--- spec/command_centre_spec.rb
+++ spec/command_centre_spec.rb
@@ -1,12 +1,17 @@
+require_relative '../commands/place'
+require_relative '../table'
+
 describe 'Command Centre' do
   before do
     @command_centre = CommandCentre.new
+    @place = Place.new
   end
 
   describe 'place command' do
     it 'issue place command' do
-      @place = Place.new
-      expect(@command_centre.execute('PLACE')).to receive(@place.execute).with(0,0,'NORTH')
+      command_array = ['PLACE',0,0,'WEST']
+      @command_centre.execute(command_array)
+      expect(Table.new.is_robot_on_table?).to be true
     end
   end
 end
