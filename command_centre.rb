cd79317a501fe0c28a20f7dfe3f47724f092bfad
Ravindra Sunimal Herath <ravisherath@gmail.com>
2016-09-20 15:47:46 +1000

    Issuing move command from command centre

--- command_centre.rb
+++ command_centre.rb
@@ -1,8 +1,12 @@
 class CommandCentre
   def execute(command_array)
-    x = command_array[1]
-    y = command_array[2]
-    facing_direction = command_array[3]
-    Place.new.execute(x,y,facing_direction)
+    if command_array[0] == 'PLACE'
+      x = command_array[1]
+      y = command_array[2]
+      facing_direction = command_array[3]
+      Place.new.execute(x,y,facing_direction)
+    elsif command_array[0] == 'MOVE'
+      Move.new.execute(facing_direction)  
+    end
   end
 end
--- spec/command_centre_spec.rb
+++ spec/command_centre_spec.rb
@@ -7,11 +7,17 @@ describe 'Command Centre' do
     @place = Place.new
   end
 
-  describe 'place command' do
+  describe 'command' do
     it 'issue place command' do
       command_array = ['PLACE',0,0,'WEST']
       @command_centre.execute(command_array)
       expect(Table.new.is_robot_on_table?).to be true
+    end
+
+    it 'issue move command only if robot on the table' do
+      command_array = ['MOVE']
+      @command_centre.execute(command_array)
+      expect(Move.new.execute('WEST')).to be nil
     end
   end
 end
