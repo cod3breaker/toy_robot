cce0ea7793d7ded30cfdcfa1c04731cc8149928d
Ravindra Sunimal Herath <ravisherath@gmail.com>
2016-09-26 09:05:06 +1000

    Refactore command-centre code

--- command_centre.rb
+++ command_centre.rb
@@ -3,29 +3,39 @@ class CommandCentre
     if command_array.empty?
       "No commands can be found"
     elsif command_array[0] == 'PLACE'
-      if command_array.length == 3
-
-        x = command_array[1]
-        y = command_array[2]
-        f = command_array[3]
-        Place.new.execute(x,y,f)
-      else
-        "Invalid number of arguments"
-      end
+      self.place_command(command_array)
     elsif self.is_robot_on_table?
-      if command_array[0] == 'MOVE'
-        Move.new.execute(Robot.instance.facing_direction)
-      elsif command_array[0] == 'LEFT'
-        Turn.new.execute('LEFT')
-      elsif command_array[0] == 'RIGHT'
-        Turn.new.execute('RIGHT')
-      elsif command_array[0] == 'REPORT'
-        Report.new.execute
-      end
+      self.other_commands(command_array)
     end
   end
 
   def is_robot_on_table?
     Table.new.is_robot_on_table?
+  end
+
+  def place_command(command_array)
+    if command_array.length == 3
+      x = command_array[1]
+      y = command_array[2]
+      f = command_array[3]
+      Place.new.execute(x,y,f)
+    else
+      "Invalid number of arguments"
+    end
+  end
+
+  def other_commands(command_array)
+    case command_array[0]
+    when 'MOVE'
+      Move.new.execute(Robot.instance.facing_direction)
+    when 'LEFT'
+      Turn.new.execute('LEFT')
+    when 'RIGHT'
+      Turn.new.execute('RIGHT')
+    when 'REPORT'
+      Report.new.execute
+    else
+      "Not a valid command"
+    end
   end
 end
