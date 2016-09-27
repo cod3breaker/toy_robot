aab3c8635c72412cc80022c894b7ac21c9b907d4
Ravindra Sunimal Herath <ravisherath@gmail.com>
2016-09-27 15:15:11 +1000

    Modifying other_commands function

--- commands/command_centre/all_other_commands.rb
+++ commands/command_centre/all_other_commands.rb
@@ -1,12 +1,22 @@
 require_relative '../move'
 require_relative '../turn'
 require_relative '../report'
+require_relative '../../table'
 
 class AllOtherCommands
   def other_commands(command_array)
     case command_array[0]
     when 'MOVE'
-      Move.new.execute(Robot.instance.facing_direction)
+      # check if Robot is falling before moving
+      x = Robot.instance.x_position
+      y = Robot.instance.y_position
+      f = Robot.instance.facing_direction
+
+      if Table.new.is_movable?(x,y,f)
+        Move.new.execute(Robot.instance.facing_direction)
+      else
+        "Cannot make a move. Robot about to fall the table."
+      end
     when 'LEFT'
       Turn.new.execute('LEFT')
     when 'RIGHT'
